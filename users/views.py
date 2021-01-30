ba93d3e9b154e68cb2fb473684aacca2c1ece010
Raul Novelo <rnovelo.cruz98@gmail.com>
2021-01-29 19:18:17 -0600

    Application custom permissions

--- users/views.py
+++ users/views.py
@@ -28,7 +28,7 @@ from .serializers import (
 
 class GroupList(generics.ListAPIView):
     """
-    A generic List API for viewing group instances.
+    A generic List API for viewing Authentication Groups.
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
@@ -36,7 +36,7 @@ class GroupList(generics.ListAPIView):
 
 class InfoAPIView(views.APIView):
     """
-    An API for viewing user and server info.
+    API for viewing user and server info.
     """
 
     def get(self, request):
@@ -60,7 +60,7 @@ class OrganizationViewSet(mixins.RetrieveModelMixin,
     """
     serializer_class = OrganizationSerializer
     queryset = Organization.objects.all()
-    permission_classes = (OrgPermissions,)
+    permission_classes = [OrgPermissions, ]
     http_method_names = ['get', 'patch', 'options', 'head']
     ordering_fields = []
     ordering = []
@@ -83,6 +83,7 @@ class UserOrganizationViewSet(viewsets.ReadOnlyModelViewSet):
             return User.objects.filter(organization=org)
         except Organization.DoesNotExist:
             return User.objects.none()
+
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -109,37 +110,14 @@ class UserViewSet(viewsets.ModelViewSet):
         org = self.request.user.organization
         return User.objects.filter(organization=org)
 
-    def list(self, request):
-        """
-        List all the users for the user organization if user is `Administrator` or `Viewer`.
-        """
-        is_admin_or_viewer = request.user.groups.filter(
-            name__in=['Administrator', 'Viewer'])
-        if not is_admin_or_viewer:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-
-        queryset = self.filter_queryset(self.get_queryset())
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
     def create(self, request):
         """
         Create an user for the organization, must set password as well.
         Request user must be `Administrator`
         """
-        is_admin = request.user.groups.filter(name__in=['Administrator'])
-        if not is_admin:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save(org=request.user.organization) # set the same org
+        serializer.save(org=request.user.organization)  # set the same org
 
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
